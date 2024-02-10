e735c06d068eca0800ee3afa4e9e77c03c083774
Florcdb <florcordoba51@gmail.com>
2024-02-10 11:50:25 +0100

    SQL

--- statements.sql
+++ statements.sql
@@ -1,159 +1,278 @@
 -- Ejercicio 1: Seleccionar todos los usuarios y sus lenguajes asociados.
--- Tu respuesta aquí
+'Juan','Java'
+'Maria','JavaScript'
+'Carlos','Python'
+'Laura','C++'
+'Pedro','Ruby'
+'Ana','SQL'
+'Miguel','PHP'
+'Sofia','Swift'
+'Javier','TypeScript'
+'Luis','Go'
+'Elena','C#'
+'Daniel','HTML'
+'Paula','CSS'
+'Alejandro','R'
+'Carmen','Kotlin'
+'Adrian','Objective-C'
+'Isabel','Scala'
+'Hector','Perl'
+'Raquel','Shell'
+'Alberto','VB.NET'
 
 
 -- Ejercicio 2: Contar cuántos usuarios hay en total.
--- Tu respuesta aquí
+20
 
 
 -- Ejercicio 3: Seleccionar los usuarios mayores de 30 años.
--- Tu respuesta aquí
+'2','Maria','Lopez','maria.lopez@example.com','32','JavaScript'
+'4','Laura','Fernandez','laura.fernandez@example.com','30','C++'
+'6','Ana','Hernandez','ana.hernandez@example.com','35','SQL'
+'9','Javier','Diaz','javier.diaz@example.com','31','TypeScript'
+'12','Daniel','Romero','daniel.romero@example.com','33','HTML'
+'16','Adrian','Molina','adrian.molina@example.com','34','Objective-C'
+'18','Hector','Ortega','hector.ortega@example.com','30','Perl'
+'19','Raquel','Serrano','raquel.serrano@example.com','32','Shell'
 
 
 -- Ejercicio 4: Contar cuántos usuarios saben JavaScript.
--- Tu respuesta aquí
+1
 
 
 -- Ejercicio 5 OPCION 2: Seleccionar los usuarios que tienen 28 años. 
--- Tu respuesta aquí
+'1','Juan','Gomez','juan.gomez@example.com','28','Java'
+'7','Miguel','Perez','miguel.perez@example.com','28','PHP'
+'14','Alejandro','Ruiz','alejandro.ruiz@example.com','28','R'
+'20','Alberto','Reyes','alberto.reyes@example.com','28','VB.NET'
 
 
 -- Ejercicio 6: Encontrar el usuario con el email 'juan.gomez@example.com'.
--- Tu respuesta aquí
+'1','Juan','Gomez','juan.gomez@example.com','28','Java'
 
 
 -- Ejercicio 7: Seleccionar los usuarios que saben Java y tienen menos de 25 años.
--- Tu respuesta aquí
-
+No hay
 
 -- Ejercicio 8 OPCION 2: Contar cuántos usuarios con edades diferentes saben el lenguaje 'Java'.
--- Tu respuesta aquí
-
+1
 
 -- Ejercicio 9: Seleccionar los usuarios que no saben ningún lenguaje.
--- Tu respuesta aquí
-
+No hay
 
 -- Ejercicio 10: Encontrar el nombre y edad del usuario más joven.
--- Tu respuesta aquí
 
 
 -- Ejercicio 11: Seleccionar los usuarios y sus edades ordenados por edad de forma descendente.
--- Tu respuesta aquí
+'Ana','35'
+'Adrian','34'
+'Daniel','33'
+'Maria','32'
+'Raquel','32'
+'Javier','31'
+'Laura','30'
+'Hector','30'
+'Elena','29'
+'Carmen','29'
+'Juan','28'
+'Miguel','28'
+'Alejandro','28'
+'Alberto','28'
+'Luis','27'
+'Sofia','26'
+'Isabel','26'
+'Carlos','25'
+'Paula','24'
+'Pedro','22'
 
 
 -- Ejercicio 12: Contar cuántos usuarios tienen más de 28 años.
--- Tu respuesta aquí
+10
 
 
 -- Ejercicio 13: Seleccionar los usuarios cuyo apellido contiene la letra 'a'.
--- Tu respuesta aquí
+'4','Laura','Fernandez','laura.fernandez@example.com','30','C++'
+'5','Pedro','Martinez','pedro.martinez@example.com','22','Ruby'
+'6','Ana','Hernandez','ana.hernandez@example.com','35','SQL'
+'8','Sofia','Garcia','sofia.garcia@example.com','26','Swift'
+'9','Javier','Diaz','javier.diaz@example.com','31','TypeScript'
+'10','Luis','Sanchez','luis.sanchez@example.com','27','Go'
+'15','Carmen','Vega','carmen.vega@example.com','29','Kotlin'
+'16','Adrian','Molina','adrian.molina@example.com','34','Objective-C'
+'18','Hector','Ortega','hector.ortega@example.com','30','Perl'
+'19','Raquel','Serrano','raquel.serrano@example.com','32','Shell'
 
 
 -- Ejercicio 14: Encontrar el lenguaje más popular entre los usuarios menores de 30 años.
--- Tu respuesta aquí
 
 
 -- Ejercicio 15: Seleccionar el usuario  mayor de 25 y que sepa el lenguaje 'TypeScript'.
--- Tu respuesta aquí
-
+'9','Javier','Diaz','javier.diaz@example.com','31','TypeScript'
 
 -- Ejercicio 16: Contar cuántos usuarios tienen un lenguaje asociado llamado 'Python'.
--- Tu respuesta aquí
-
+1
 
 -- Ejercicio 17: Seleccionar los usuarios y sus lenguajes asociados, si tienen alguno, ordenados por nombre de usuario.
--- Tu respuesta aquí
+
 
 
 -- Ejercicio 18: Encontrar los usuario con el email que contiene la palabra 'example'.
--- Tu respuesta aquí
+'1','Juan','Gomez','juan.gomez@example.com','28','Java'
+'2','Maria','Lopez','maria.lopez@example.com','32','JavaScript'
+'3','Carlos','Rodriguez','carlos.rodriguez@example.com','25','Python'
+'4','Laura','Fernandez','laura.fernandez@example.com','30','C++'
+'5','Pedro','Martinez','pedro.martinez@example.com','22','Ruby'
+'6','Ana','Hernandez','ana.hernandez@example.com','35','SQL'
+'7','Miguel','Perez','miguel.perez@example.com','28','PHP'
+'8','Sofia','Garcia','sofia.garcia@example.com','26','Swift'
+'9','Javier','Diaz','javier.diaz@example.com','31','TypeScript'
+'10','Luis','Sanchez','luis.sanchez@example.com','27','Go'
+'11','Elena','Moreno','elena.moreno@example.com','29','C#'
+'12','Daniel','Romero','daniel.romero@example.com','33','HTML'
+'13','Paula','Torres','paula.torres@example.com','24','CSS'
+'14','Alejandro','Ruiz','alejandro.ruiz@example.com','28','R'
+'15','Carmen','Vega','carmen.vega@example.com','29','Kotlin'
+'16','Adrian','Molina','adrian.molina@example.com','34','Objective-C'
+'17','Isabel','Gutierrez','isabel.gutierrez@example.com','26','Scala'
+'18','Hector','Ortega','hector.ortega@example.com','30','Perl'
+'19','Raquel','Serrano','raquel.serrano@example.com','32','Shell'
+'20','Alberto','Reyes','alberto.reyes@example.com','28','VB.NET'
 
 
 -- Ejercicio 19: Seleccionar los usuarios que saben al menos un lenguaje y tienen una edad entre 25 y 35 años.
--- Tu respuesta aquí
+'1','Juan','Gomez','juan.gomez@example.com','28','Java'
+'2','Maria','Lopez','maria.lopez@example.com','32','JavaScript'
+'4','Laura','Fernandez','laura.fernandez@example.com','30','C++'
+'7','Miguel','Perez','miguel.perez@example.com','28','PHP'
+'8','Sofia','Garcia','sofia.garcia@example.com','26','Swift'
+'9','Javier','Diaz','javier.diaz@example.com','31','TypeScript'
+'10','Luis','Sanchez','luis.sanchez@example.com','27','Go'
+'11','Elena','Moreno','elena.moreno@example.com','29','C#'
+'12','Daniel','Romero','daniel.romero@example.com','33','HTML'
+'14','Alejandro','Ruiz','alejandro.ruiz@example.com','28','R'
+'15','Carmen','Vega','carmen.vega@example.com','29','Kotlin'
+'16','Adrian','Molina','adrian.molina@example.com','34','Objective-C'
+'17','Isabel','Gutierrez','isabel.gutierrez@example.com','26','Scala'
+'18','Hector','Ortega','hector.ortega@example.com','30','Perl'
+'19','Raquel','Serrano','raquel.serrano@example.com','32','Shell'
+'20','Alberto','Reyes','alberto.reyes@example.com','28','VB.NET'
+
+
 
 
 -- Ejercicio 20: Contar cuántos usuarios tienen un lenguaje asociado llamado 'CSS' y tienen menos de 30 años.
--- Tu respuesta aquí
-
+'13','Paula','Torres','paula.torres@example.com','24','CSS'
 
 -- Ejercicio 21: Seleccionar los usuarios que tienen al menos un lenguaje asociado y mostrar la cantidad de lenguajes que tienen.
--- Tu respuesta aquí
+
 
 
 -- Ejercicio 22: Encontrar el lenguaje con más caracteres.
--- Tu respuesta aquí
+'JavaScript'
 
 
 -- Ejercicio 23: Seleccionar los usuarios y mostrar la concatenación de su nombre y apellido.
--- Tu respuesta aquí
 
 
 -- Ejercicio 24: Contar cuántos lenguajes diferentes conocen los usuarios mayores de 25 años. 
--- Tu respuesta aquí
+'19'
 
 
 -- Ejercicio 25: Seleccionar los usuarios que tienen exactamente la misma edad.
--- Tu respuesta aquí
+'Miguel', 
+
 
 
 -- Ejercicio 26: Encontrar el usuario con el lenguaje con mayor número de carácteres y que tenga una edad menor de 30 años. 
--- Tu respuesta aquí
+'Sofia', 'Garcia', '26','Swift'
 
 
 -- Ejercicio 27: Seleccionar los usuarios que tienen al menos un lenguaje asociado y mostrar sus emails.
--- Tu respuesta aquí
+'1','Juan','Gomez','juan.gomez@example.com','28','Java'
+'2','Maria','Lopez','maria.lopez@example.com','32','JavaScript'
+'3','Carlos','Rodriguez','carlos.rodriguez@example.com','25','Python'
+'4','Laura','Fernandez','laura.fernandez@example.com','30','C++'
+'5','Pedro','Martinez','pedro.martinez@example.com','22','Ruby'
+'7','Miguel','Perez','miguel.perez@example.com','28','PHP'
+'8','Sofia','Garcia','sofia.garcia@example.com','26','Swift'
+'9','Javier','Diaz','javier.diaz@example.com','31','TypeScript'
+'10','Luis','Sanchez','luis.sanchez@example.com','27','Go'
+'11','Elena','Moreno','elena.moreno@example.com','29','C#'
+'12','Daniel','Romero','daniel.romero@example.com','33','HTML'
+'13','Paula','Torres','paula.torres@example.com','24','CSS'
+'14','Alejandro','Ruiz','alejandro.ruiz@example.com','28','R'
+'15','Carmen','Vega','carmen.vega@example.com','29','Kotlin'
+'16','Adrian','Molina','adrian.molina@example.com','34','Objective-C'
+'17','Isabel','Gutierrez','isabel.gutierrez@example.com','26','Scala'
+'18','Hector','Ortega','hector.ortega@example.com','30','Perl'
+'19','Raquel','Serrano','raquel.serrano@example.com','32','Shell'
+'20','Alberto','Reyes','alberto.reyes@example.com','28','VB.NET'
 
 
 -- Ejercicio 28: Contar cuántos usuarios tienen un apellido que comienza con la letra 'G'.
--- Tu respuesta aquí
+2
 
 
 -- Ejercicio 29: Seleccionar los usuarios que tienen lenguajes que empiecen por 'J' y mostrar solo el que tiene menos caracteres. 
--- Tu respuesta aquí
+'Maria', 'Lopez', '10'
 
 
 -- Ejercicio 30: Encontrar el usuario con edad mayor a 30 y que sabe el lenguaje con menos caracteres.
--- Tu respuesta aquí
+'Ana', 'Hernandez', '35', 'SQL'
 
 
 -- Ejercicio 31: Seleccionar los usuarios que saben al menos un lenguaje y ordenarlos por nombre de lenguaje. 
--- Tu respuesta aquí
 
 
 -- Ejercicio 32: Contar cuántos usuarios tienen una edad entre 20 y 25 años y saben al menos un lenguaje. 
--- Tu respuesta aquí
-
+4
 
 -- Ejercicio 33: Seleccionar los usuarios que no tienen un lenguaje asociado llamado 'SQL'.
--- Tu respuesta aquí
+'1','Juan','Gomez','juan.gomez@example.com','28','Java'
+'2','Maria','Lopez','maria.lopez@example.com','32','JavaScript'
+'3','Carlos','Rodriguez','carlos.rodriguez@example.com','25','Python'
+'4','Laura','Fernandez','laura.fernandez@example.com','30','C++'
+'5','Pedro','Martinez','pedro.martinez@example.com','22','Ruby'
+'7','Miguel','Perez','miguel.perez@example.com','28','PHP'
+'8','Sofia','Garcia','sofia.garcia@example.com','26','Swift'
+'9','Javier','Diaz','javier.diaz@example.com','31','TypeScript'
+'10','Luis','Sanchez','luis.sanchez@example.com','27','Go'
+'11','Elena','Moreno','elena.moreno@example.com','29','C#'
+'12','Daniel','Romero','daniel.romero@example.com','33','HTML'
+'13','Paula','Torres','paula.torres@example.com','24','CSS'
+'14','Alejandro','Ruiz','alejandro.ruiz@example.com','28','R'
+'15','Carmen','Vega','carmen.vega@example.com','29','Kotlin'
+'16','Adrian','Molina','adrian.molina@example.com','34','Objective-C'
+'17','Isabel','Gutierrez','isabel.gutierrez@example.com','26','Scala'
+'18','Hector','Ortega','hector.ortega@example.com','30','Perl'
+'19','Raquel','Serrano','raquel.serrano@example.com','32','Shell'
+'20','Alberto','Reyes','alberto.reyes@example.com','28','VB.NET'
+
 
 
 -- Ejercicio 34: Encontrar el lenguaje con más caracteres entre los usuarios que tienen al menos 30 años.
--- Tu respuesta aquí
+'JavaScript'
 
 
 -- Ejercicio 35: Seleccionar los usuarios y mostrar la diferencia entre su edad y la edad promedio de todos los usuarios
--- Tu respuesta aquí
+
 
 
 -- Ejercicio 36: Contar cuántos usuarios tienen un lenguaje asociado que contiene la palabra 'Script'.
--- Tu respuesta aquí
-
+2
 
 -- Ejercicio 37: Seleccionar los usuarios que tienen al menos un lenguaje asociado y mostrar la longitud de su nombre. 
--- Tu respuesta aquí
+
 
 
 -- Ejercicio 38: Encontrar el lenguaje del usuario creado más tarde. 
--- Tu respuesta aquí
+'VB.NET'
 
 
 -- Ejercicio 39: Seleccionar los usuarios y mostrar la suma de sus edades. 
--- Tu respuesta aquí
+529
 
 
 -- Ejercicio 40: Contar cuántos usuarios tienen un lenguaje asociado que comienza con la letra 'P' y tienen menos de 28 años.
--- Tu respuesta aquí
+1
 
